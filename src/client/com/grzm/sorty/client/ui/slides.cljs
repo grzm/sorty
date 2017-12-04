77a473978f4e68b888bb34492ab0bc2ae5e277a3
Michael Glaesemann <grzm@seespotcode.net>
2017-12-04 12:27:38 -0600

    Compare two slide variants (radio vs button)

--- src/client/com/grzm/sorty/client/ui/slides.cljs
+++ src/client/com/grzm/sorty/client/ui/slides.cljs
@@ -6,9 +6,51 @@
 (defui Slide
   Object
   (render [this]
-    (let [{:keys [text id]} (om/props this)]
-      (dom/div #js {:id id}
-               (dom/p nil text)
-               (dom/form)))))
+    (let [{:keys [text id s-class]} (om/props this)
+          default-label-attrs       {:className "form-check-label"}
+          default-input-attrs       {:className "btn"
+                                     :name      "s-class"
+                                     :type      "radio"}
+          radio-button              (fn [label-attrs input-attrs text]
+                                      (dom/label
+                                        (clj->js (merge default-label-attrs
+                                                        label-attrs))
+                                        (dom/input (clj->js (merge default-input-attrs
+                                                                   input-attrs))) text))]
+      (dom/div
+        #js {:id id}
+        (dom/p nil text)
+        (dom/form
+          nil
+          (dom/fieldset
+            nil
+            (dom/legend nil "Is this " (dom/strong nil s-class) "?")
+            (dom/div
+              #js {:className "form-check"}
+              (radio-button {:accessKey ","} {:value "yes"} "yes")
+              (radio-button {:accessKey "."} {:value "no"} "no")
+              (radio-button {:accessKey "s"} {:value "skip"} "skip")
+              (dom/button
+                #js {:className "btn btn-primary" :type "submit" :value "Submit"} "Submit"))))))))
 
 (def ui-slide (om/factory Slide {:keyfn :id}))
+
+(defui SlideWithButtons
+  Object
+  (render [this]
+    (let [{:keys [text id s-class]} (om/props this)]
+      (dom/div
+        #js {:id id}
+        (dom/p nil text)
+        (dom/form
+          nil
+          (dom/fieldset
+            nil
+            (dom/legend nil "Is this " (dom/strong nil s-class) "?")
+            (dom/div
+              #js {:className "form-check"}
+              (dom/button #js {:className "btn btn-primary" :type "button" :value "yes"} "yes")
+              (dom/button #js {:className "btn btn-primary" :type "button" :value "no"} "no")
+              (dom/button #js {:className "btn btn-primary" :type "button" :value "skip"} "skip"))))))))
+
+(def ui-slide-with-buttons (om/factory SlideWithButtons {:keyfn :id}))
--- src/devcards/com/grzm/sorty/devcards/slides.cljs
+++ src/devcards/com/grzm/sorty/devcards/slides.cljs
@@ -3,5 +3,12 @@
    [com.grzm.sorty.client.ui.slides :as slides]
    [devcards.core :refer-macros [defcard]]))
 
+(def slide-data {:id      3
+                 :text    "Here's some text"
+                 :s-class "spam"})
+
 (defcard display-slide
-  (slides/ui-slide {:id 3 :text "Here's some text"}))
+  (slides/ui-slide slide-data))
+
+(defcard display-slide-with-buttons
+  (slides/ui-slide-with-buttons slide-data))
