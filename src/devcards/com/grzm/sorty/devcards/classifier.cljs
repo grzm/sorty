e9f9075658445c5c233e8cdabdab2334166d58ef
Michael Glaesemann <grzm@seespotcode.net>
2017-12-10 17:55:51 -0600

    Refactored classification list, now with normalization.
    Removed TextItem component to more closely parallel
    Getting Started docs, as normalization wasn't working
    with the components I had.

--- src/devcards/com/grzm/sorty/devcards/classifier.cljs
+++ src/devcards/com/grzm/sorty/devcards/classifier.cljs
@@ -8,47 +8,26 @@
    [fulcro.client.mutations :as m :refer [defmutation]]
    [fulcro.client.primitives :as prim :refer-macros [defui]]))
 
-(defui ^:once ClassifiableTextItemRoot
-  static prim/IQuery
-  (query [this]
-    [:ui/react-key {:item (prim/get-query classifier/ClassifiableTextItem)}])
-
-  Object
-  (render [this]
-    (let [{:keys [ui/react-key item]} (prim/props this)]
-      (dom/div
-        #js {:key react-key}
-        (classifier/ui-classifiable-text-item item)))))
-
-(defcard-fulcro classifiable-text-item
-  ClassifiableTextItemRoot
-  {:item {:s-class   {:id 4 :name "spam"}
-          :text-item {:id 1 :text "Here's some text"}}}
-  {:inspect-data true})
-
 (defui ^:once ClassifiableTextItemListRoot
   static prim/IQuery
-  (query [this]
-    [:ui/react-key {:unclassified (prim/get-query classifier/ClassifiableTextItemList)}])
+  (query
+    [this]
+    [:ui/react-key
+     {:unclassified (prim/get-query classifier/ClassifiableTextItemList)}])
+
+  static prim/InitialAppState
+  (initial-state [c params]
+                 {:unclassified (prim/get-initial-state classifier/ClassifiableTextItemList {:item-list/id :unclassified})})
 
   Object
-  (render [this]
+  (render
+    [this]
     (let [{:keys [ui/react-key unclassified]} (prim/props this)]
       (dom/div
         #js {:key react-key}
         (classifier/ui-classifiable-text-item-list unclassified)))))
 
-(defcard-fulcro classification-text-item-list
+(defcard-fulcro text-item-list
   ClassifiableTextItemListRoot
-  {:unclassified
-   {:item-list/id    :unclassified
-    :item-list/items [{:s-class   {:id 4 :name "spam"}
-                       :text-item {:id 1 :text "Here's some text"}}
-                      {:s-class   {:id 4 :name "spam"}
-                       :text-item {:id 2 :text "Here's some other text"}}
-                      {:s-class   {:id 4 :name "spam"}
-                       :text-item {:id 3 :text "Hey, this is text, too"}}]}
-   :classified
-   {:item-list/id    :classified
-    :item-list/items []}}
+  nil
   {:inspect-data true})
