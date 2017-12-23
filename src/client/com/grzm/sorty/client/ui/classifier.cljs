5b0cf2e649ac85d09b98daf82333bb1e4db74917
Michael Glaesemann <grzm@seespotcode.net>
2017-12-22 20:00:08 -0600

    Initial cut of keyboard shortcuts.

--- src/client/com/grzm/sorty/client/ui/classifier.cljs
+++ src/client/com/grzm/sorty/client/ui/classifier.cljs
@@ -3,7 +3,9 @@
    [fulcro.client.dom :as dom]
    [fulcro.client.logging :as log]
    [fulcro.client.mutations :as m :refer [defmutation]]
-   [fulcro.client.primitives :as prim :refer-macros [defui]]))
+   [fulcro.client.primitives :as prim :refer-macros [defui]]
+   [goog.events :as events])
+  (:import [goog.ui KeyboardShortcutHandler]))
 
 (defui ^:once ClassifiableTextItem
   static prim/Ident
@@ -64,21 +66,56 @@
     (prim/transact! c `[(classify-item
                           {:list-id ~list-id :item-id ~item-id :class-id ~class-id :value ~value})])))
 
+;; Does it matter that I'm instantiating a new handler each time?
+;; Do they get overwritten?
+;; Will this work with key combinations? (passing an array as the first arg?)
+(defn install-shortcut!
+  "Installs a keyboard shortcut handler
+  The key is a string the trigger is a function that will receive the keyboard event as the
+  first argument. If once? is true the keyboard shortcut is only fired once.
+  The unregister handler is returned and can be called to unregister the listener.
+
+  https://gist.github.com/rauhs/ec1a7b94a6481ae4cf1d"
+  ([key trigger] (install-shortcut! key trigger true))
+  ([key trigger once?]
+    (let [identifier (str key once?)
+          handler (KeyboardShortcutHandler. js/document)]
+      (.registerShortcut handler identifier key)
+      (events/listen handler KeyboardShortcutHandler.EventType.SHORTCUT_TRIGGERED
+                     (fn [e]
+                       (trigger e)
+                       (when once?
+                         (.unregisterShortcut handler key))))
+      (fn [] (.unregisterShortcut handler key)))))
+
 (defui ^:once ClassifiableTextItemList
   static prim/Ident
   (ident [c {:keys [item-list/id]}]
          [:item-list/by-id id])
 
   static prim/IQuery
-  (query [this] [:item-list/id {:item-list/items (prim/get-query ClassifiableTextItem)}])
+  (query [_this] [:item-list/id {:item-list/items (prim/get-query ClassifiableTextItem)}])
 
   static prim/InitialAppState
   (initial-state
     [c {:keys [item-list/id item-list/items]}]
     {:item-list/id    id
      :item-list/items []})
-
   Object
+  (componentDidMount [this]
+    (let [{:keys [item-list/id item-list/items]} (prim/props this)
+          first-item (first items)
+          {:keys [text-item s-class]} first-item
+          classify-fn (make-classify-fn this id)]
+      (prim/set-state! this {:kb-handler (install-shortcut!
+                                           "Y"
+                                           (fn [e]
+                                             (log/info (prn {:identifier (.-identifier e)
+                                                             :item       first-item}))
+                                             (classify-fn text-item s-class :yes))
+                                           false)})))
+  (componentWillUnmount [this]
+    ((prim/get-state this :kb-handler)))
   (render [this]
           (let [{:keys [item-list/id item-list/items]} (prim/props this)
                 classify-fn (make-classify-fn this id)]
